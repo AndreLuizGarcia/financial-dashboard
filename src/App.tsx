3a6963b5a95d3f58972faa066534a5ed510ae4fe
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2023-11-10 10:03:56 -0300

    feat: entrypoint application

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import InitialLoading from './components/InitialLoading';
+import Footer from './components/Layout/Footer';
+import Header from './components/Layout/Header';
+import Main from './components/Layout/Main';
+import MainGridTemplate from './components/Layout/MainGridTemplate';
+import useLoadData from './hooks/useLoadData';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { isLoading } = useLoadData();
+
+  return isLoading ? (
+    <InitialLoading />
+  ) : (
+    <MainGridTemplate>
+      <Header />
+      <Main />
+      <Footer />
+    </MainGridTemplate>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
